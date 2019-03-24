7993f98f268efb0343557112a97e0bbf3131dd02
daniel <daniel@kasmweb.com>
2019-03-24 00:04:13 -0400

    Adding basic spring boot setup

---
+++ budget/src/main/java/budget/BudgetStart.java
@@ -0,0 +1,15 @@
+package budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetStart.class, args);
+		
+		
+	}
+
+}
---
+++ budget/src/main/java/budget/hello/HelloController.java
@@ -0,0 +1,16 @@
+package budget.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "HI";
+		
+	}
+	
+	
+}
