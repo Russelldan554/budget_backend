0fdb81602230ad5b80c9e5cf66159f05d21142de
daniel <daniel@kasmweb.com>
2019-03-28 23:20:15 -0400

    Initial Commit of Rolands Starter

---
+++ src/main/java/budget/api/BudgetApiAppApplication.java
@@ -0,0 +1,13 @@
+package budget.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApiAppApplication.class, args);
+	}
+
+}
