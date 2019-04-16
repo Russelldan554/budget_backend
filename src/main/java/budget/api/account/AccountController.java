7dca51874c406fd313ff9eb99eb437af3fa299a6
muayuechi <muayuechi@yahoo.com>
2019-04-15 23:55:10 -0500

    Added Cross Origins at class level for each controller

--- src/main/java/budget/api/account/AccountController.java
+++ src/main/java/budget/api/account/AccountController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import budget.api.user.User;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class AccountController {
     
--- src/main/java/budget/api/transaction/TransactionController.java
+++ src/main/java/budget/api/transaction/TransactionController.java
@@ -3,6 +3,7 @@ package budget.api.transaction;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import budget.api.account.Account;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class TransactionController {
     
